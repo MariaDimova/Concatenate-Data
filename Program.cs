b5be0497cb2431e6b95a4eda873eafb5af5df89e
MariaDimova <150257957+MariaDimova@users.noreply.github.com>
2023-11-19 17:59:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concatenate_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // “You are <firstName> <lastName>, a <age>-years old person from <town>”.
+
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string town = Console.ReadLine();
+
+            Console.WriteLine($"You are {firstName} { lastName}, a { age}-years old person from { town}.");
+
+        }
+    }
+}
